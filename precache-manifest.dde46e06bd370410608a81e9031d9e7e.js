e300602417e687c18f9a769a9926ec3b2da10cb3
Ben Davie <benjamindavie6@gmail.com>
2020-08-06 13:29:21 +0100

    Updates

---
+++ precache-manifest.dde46e06bd370410608a81e9031d9e7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d2e530fee23708ef37141fa47511b6",
+    "url": "/udemy-course-widget-app/index.html"
+  },
+  {
+    "revision": "7cc4ae36b3239678a882",
+    "url": "/udemy-course-widget-app/static/js/2.4088855b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemy-course-widget-app/static/js/2.4088855b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d302a553db10506ca8",
+    "url": "/udemy-course-widget-app/static/js/main.8184392b.chunk.js"
+  },
+  {
+    "revision": "a8c8060dec8e3a3ba1a4",
+    "url": "/udemy-course-widget-app/static/js/runtime-main.261aff82.js"
+  }
+]);
